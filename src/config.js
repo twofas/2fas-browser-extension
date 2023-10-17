39b3bf81597f51662763da4f0b0889d066b75057
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2023-10-17 16:51:44 +0200

    #1364 Packages update, v1.4.6

--- src/config.js
+++ src/config.js
@@ -23,7 +23,7 @@ const t = require('./_locales/en/notifications.json');
 const config = {
   WebSocketTimeout: 3, // in minutes
   ResendPushTimeout: 10, // in seconds
-  ExtensionVersion: '1.4.5',
+  ExtensionVersion: '1.4.6',
 
   Texts: {
     Error: {
