fbfe4864da4dbe97d4828d729e605d8d8c8c6fe9
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2024-01-11 17:57:01 +0100

    #1458 includes FIX

--- src/partials/storeLog.js
+++ src/partials/storeLog.js
@@ -89,10 +89,10 @@ const storeLog = async (level, logID = 0, errObj, url = '') => {
     (storage?.browserInfo?.browser_name === 'Firefox' && storage?.browserInfo?.browser_version === '105.0' && logID === 14) ||
     (storage?.browserInfo?.browser_name === 'Chrome' && storage?.browserInfo?.browser_version === '107' && logID === 14) ||
     (storage?.browserInfo?.browser_name === 'Chrome' && storage?.browserInfo?.browser_version === '107.0.0.0' && logID === 14) ||
-    (c?.errorInfo?.message.includes('FILE_ERROR_NO_SPACE')) ||
+    (c?.errorInfo?.message?.includes('FILE_ERROR_NO_SPACE')) ||
     (c?.status === 407) ||
-    (c?.errorInfo?.message.includes('An unexpected error occurred')) ||
-    (c?.errorInfo?.message.includes('Refused to run the JavaScript URL'))
+    (c?.errorInfo?.message?.includes('An unexpected error occurred')) ||
+    (c?.errorInfo?.message?.includes('Refused to run the JavaScript URL'))
   ) {
     return false;
   }
