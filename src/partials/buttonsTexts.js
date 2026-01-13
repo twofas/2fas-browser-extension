c74d1e9a5b930aac817842e49f9db2af80ade22f
Greg Zajac <grzegorz.zajac000@gmail.com>
2026-01-13 12:08:13 +0100

    2023 -> 2026

--- src/partials/buttonsTexts.js
+++ src/partials/buttonsTexts.js
@@ -1,6 +1,6 @@
 //
 //  This file is part of the 2FAS Browser Extension (https://github.com/twofas/2fas-browser-extension)
-//  Copyright © 2023 Two Factor Authentication Service, Inc.
+//  Copyright © 2026 Two Factor Authentication Service, Inc.
 //  Contributed by Grzegorz Zając. All rights reserved.
 //
 //  This program is free software: you can redistribute it and/or modify
