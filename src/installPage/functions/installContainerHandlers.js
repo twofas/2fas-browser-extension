bc293df817fbe759bb4b6206359d7bb14d9794ac
Greg Zajac <grzegorz.zajac000@gmail.com>
2026-01-15 11:19:42 +0100

    ESLint - global Websocket added

--- src/installPage/functions/installContainerHandlers.js
+++ src/installPage/functions/installContainerHandlers.js
@@ -17,7 +17,7 @@
 //  along with this program. If not, see <https://www.gnu.org/licenses/>
 //
 
-/* global CustomEvent */
+/* global CustomEvent, WebSocket */
 import S from '@/selectors.js';
 import setQRCode from '@installPage/functions/setQRCode.js';
 
