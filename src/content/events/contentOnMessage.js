bc68231565ed4f71b78a5e11cd7d7ecbdd534c3c
Greg Zajac <grzegorz.zajac000@gmail.com>
2026-01-16 10:44:23 +0100

    InputToken/TokenNotification FIX

--- src/content/events/contentOnMessage.js
+++ src/content/events/contentOnMessage.js
@@ -111,7 +111,7 @@ const contentOnMessage = (request, sender, sendResponse, tabData) => {
           }
         }
 
-        sendResponse(inputToken(request, tokenInput, tabData?.url));
+        sendResponse(await inputToken(request, tokenInput, tabData?.url));
 
         sessionTabData = null;
       })();
